517ed2f3c277308c4bd0e2f58ba18252bc1f0b18
Ananda Rizky Daffana <daffa20rizky@gmail.com>
2024-06-13 23:58:45 +0800

    hdhv

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const Hapi = require("@hapi/hapi");
 const routes = require("./routes");
 const init = async () => {
     const server = Hapi.server({
-        port: 9000,
+        port: 8000,
         host: "localhost",
         routes: {
             cors: {
