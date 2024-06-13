9dce2e62193deb38eed6347d17b3f3d92980e552
Ananda Rizky Daffana <daffa20rizky@gmail.com>
2024-06-14 00:25:46 +0800

    change to 9000

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const Hapi = require("@hapi/hapi");
 const routes = require("./routes");
 const init = async () => {
     const server = Hapi.server({
-        port: 8000,
+        port: 9000,
         host: "localhost",
         routes: {
             cors: {
